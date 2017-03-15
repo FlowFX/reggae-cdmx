9a215c4aa50011fb5a4367e131cef312ee9b1ac1
Florian Posdziech <hallo@flowfx.de>
2017-03-14 18:16:37 -0600

    wip

--- reggae_cdmx/tests/test_forms.py
+++ reggae_cdmx/tests/test_forms.py
@@ -7,19 +7,13 @@ from ..forms import EventForm
 from datetime import date
 
 
-# @pytest.mark.parametrize('title, venue, date, validity', [
-#     ('Bungalo Dub ft. Jahshua Soundman', 1, date(2017, 8, 20), True),
-    # ('Bungalo Dub ft. Jahshua Soundman', 'Kaliman Bar', '20/08/2017', True),
-    # ('Bungalo Dub', 'Kaliman Bar', '', False),   # empty date
-    # ('', 'Kaliman Bar', '20/08/2017', False),    # empty title
-    # ('Bungalo Dub ft. Jahshua Soundman', '', '20/08/2017', False),      # empty venue
-    # ])
-@pytest.mark.django_db
 @pytest.mark.parametrize('title, date, validity', [
     ('Bungalo Dub ft. Jahshua Soundman', date(2017, 8, 20), True),
+    ('Bungalo Dub ft. Jahshua Soundman', '20/08/2017', True),
+    ('Bungalo Dub', '', False),   # empty date
+    ('', '20/08/2017', False),    # empty title
     ])
 def test_event_create_form(title, date, validity):
-    VenueFactory.create_batch(5)
     """Test form validation for EventForm."""
     form = EventForm(data={
         'title': title,
