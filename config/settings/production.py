fc1e102846fd3c2355b3ca78228996a065a411e8
Florian Posdziech <hallo@flowfx.de>
2017-03-21 07:58:32 -0600

    Let's allow PythonAnywhere to host our site

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,3 +5,5 @@ STATIC_ROOT = '/var/www/static/reggae-cdmx.com/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/media/reggae-cdmx.com/'
+
+ALLOWED_HOSTS = ['dev.reggae-cdmx.com', 'www.reggae-cdmx.com']
