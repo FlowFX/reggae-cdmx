8bd67ae7ebc443e5892b773f285ebaa98083fe17
Florian Posdziech <hallo@flowfx.de>
2017-03-21 07:36:00 -0600

    Configure static and media root

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,13 +1,5 @@
 from .common import *
 
-DEBUG = True
-
-INSTALLED_APPS += ['debug_toolbar',]
-
-
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
-
-
 STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/static/reggae-cdmx.com/'
 
