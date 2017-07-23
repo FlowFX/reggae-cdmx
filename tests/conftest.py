b0d3fb0df034bd06d19719d15624ad0f52fdd354
Florian Posdziech <hallo@flowfx.de>
2017-07-23 18:24:33 -0500

    test: System test uses authenticated users if necessary

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,7 @@ def user():
     user = event_factories.UserFactory.create(
         username='testuser',
         password=make_password('password'),
-        )
+    )
 
     return user
 
@@ -37,17 +37,18 @@ def user():
 @pytest.fixture()
 def authenticated_user(db, client, user):
     """Return an authenticated user."""
+    # TODO: don't use the database here, if possible
     client.force_login(user)
 
     return user
 
 
 @pytest.fixture()
-def cookie(client, user):
+def cookie(client, authenticated_user):
     """Return a session cookie for a logged-in user."""
     # http://django.readthedocs.io/en/1.9.x/topics/testing/tools.html#django.test.Client.force_login
     # https://stackoverflow.com/a/39742798/6476946
     client.force_login(user)
     cookie = client.cookies['sessionid']
 
-    return cookie
+    yield cookie
