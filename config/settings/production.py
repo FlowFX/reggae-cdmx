3cfc6c0d2c0178dbd48c1ad1ae89da72096192b9
Florian Posdziech <hallo@flowfx.de>
2017-07-26 12:56:26 -0500

    chore: Update production config

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,4 +6,5 @@ STATIC_ROOT = '/var/www/static/reggae-cdmx.com/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/media/reggae-cdmx.com/'
 
-ALLOWED_HOSTS = ['dev.reggae-cdmx.com', 'www.reggae-cdmx.com']
+ALLOWED_HOSTS = ['.reggae-cdmx.com', ]
+INTERNAL_IPS = []
