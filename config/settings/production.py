76cf24714c7ac3279ab0b826e4beeb44f6c2646c
Florian Posdziech <hallo@flowfx.de>
2017-07-26 17:44:17 -0500

    Configure mysql in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,16 +19,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.abspath('/var/www/media/reggae-cdmx.com/')
 
 # # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': get_secret('DB_NAME'),
-#         'USER': get_secret('DB_USER'),
-#         'PASSWORD': get_secret('DB_PASSWORD'),
-#         'HOST': get_secret('DB_HOST'),
-#         'PORT': get_secret('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('DB_USER'),
+        'PASSWORD': get_secret('DB_PASSWORD'),
+        'HOST': get_secret('DB_HOST'),
+        # 'PORT': get_secret('DB_PORT'),
+    }
+}
 
 # Caching
 CACHES = {
