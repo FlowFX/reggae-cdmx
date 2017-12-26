29944c81ce74f9c5709c71ecb96de40db54f772c
Florian Posdziech <hallo@flowfx.de>
2017-12-26 10:02:54 -0600

    Fix Travis CI script deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-cd ..
-# start ssh agent
-eval "$(ssh-agent -s)"
-chmod 600 deploy_key
-ssh-add deploy_key
-# configure remote repository
-git remote add pythonanywhere flowfx@ssh.pythonanywhere.com:/home/flowfx/bare-repos/reggae-cdmx.git
 # push master branch to production 
 git push -f pythonanywhere master
